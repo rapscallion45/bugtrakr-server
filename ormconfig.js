9882d1a879d72674c98a8728ffb8f4cad563d2a0
Carl <carl.scrivener.89@gmail.com>
2022-07-17 20:15:14 +0100

    purge typeorm TypeScript config, add updated db schema

--- ormconfig.js
+++ ormconfig.js
@@ -10,16 +10,8 @@ module.exports = {
   url:
     process.env.DATABASE_URL ||
     `${type}://${username}:${password}@${host}:${port}/${database}`,
-  entities: [
-    process.env.NODE_ENV === "test"
-      ? "src/entity/**/*.ts"
-      : "build/entity/**/*.js",
-  ],
-  migrations: [
-    process.env.NODE_ENV === "test"
-      ? "src/migration/**/*.ts"
-      : "build/migration/**/*.js",
-  ],
+  entities: ["build/entity/**/*.js"],
+  migrations: ["build/migration/**/*.js"],
   cli: {
     entitiesDir:
       process.env.NODE_ENV === "test" ? "src/entity" : "build/entity",
